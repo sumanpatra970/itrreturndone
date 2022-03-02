eac3868d9f9d0a18e25b2e46c12bc572f458e84c
sumanpatra970 <patrasuman992@gmail.com>
2022-03-02 20:04:27 +0530

    Update urls.py

--- itr/urls.py
+++ itr/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home),
     path('signup',views.signup),
     path('account',views.account),
 ]
+urlpatterns += staticfiles_urlpatterns()
