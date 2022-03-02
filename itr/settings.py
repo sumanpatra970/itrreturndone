977e0bcf5438fcb5af5edc6beb57b9c28327ba42
sumanpatra970 <patrasuman992@gmail.com>
2022-03-02 20:46:39 +0530

    Update settings.py

--- itr/settings.py
+++ itr/settings.py
@@ -77,10 +77,12 @@ WSGI_APPLICATION = 'itr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
 
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
